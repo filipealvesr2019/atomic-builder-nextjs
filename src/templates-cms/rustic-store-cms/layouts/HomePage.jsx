b8f85f23621525fad810d05afab53fcc75b4add6
filipealvesr2019 <filipealvesrds2019@gmail.com>
2025-12-06 16:30:26 -0300

    add

--- src/templates-cms/rustic-store-cms/layouts/HomePage.jsx
+++ src/templates-cms/rustic-store-cms/layouts/HomePage.jsx
@@ -1,22 +1,26 @@
+import Header from '../sections/Header';
 import Hero from '../sections/Hero';
 import Products from '../sections/Products';
 import About from '../sections/About';
 import Newsletter from '../sections/Newsletter';
 import Contact from '../sections/Contact';
+import Footer from '../sections/Footer';
 
 export default function HomePage({ sections = {} }) {
   return (
     <div>
+      <Header {...(sections.header || {})} />
       <Hero {...(sections.hero || {})} />
       <Products {...(sections.products || {})} />
       <About {...(sections.about || {})} />
       <Newsletter {...(sections.newsletter || {})} />
       <Contact {...(sections.contact || {})} />
+      <Footer {...(sections.footer || {})} />
     </div>
   );
 }
 
 HomePage.cmsConfig = {
   name: "Página Inicial",
-  sections: ['hero', 'products', 'about', 'newsletter', 'contact']
+  sections: ['header', 'hero', 'products', 'about', 'newsletter', 'contact', 'footer']
 };
