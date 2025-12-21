38d6aabd9536d751f08c29e46ceb884c2a890f20
filipealvesr2019 <filipealvesrds2019@gmail.com>
2025-12-21 11:55:33 -0300

    add

--- src/templates-cms/ursula-theme/sections/ClientLove.jsx
+++ src/templates-cms/ursula-theme/sections/ClientLove.jsx
@@ -5,11 +5,12 @@ import styles from './ClientLove.module.css';
 export default function ClientLove({
   quote = "Ursula is a fantastic theme. It helped me create the blog of my dreams in minutes. The design is elegant and the features are just what I needed.",
   author = "Sarah J.",
-  image = "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?w=500&q=80"
+  image = "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?w=500&q=80",
+  forceMobile
 }) {
   return (
     <section className={styles.section}>
-      <div className={`${styles.container} ursula-mobile-stack`}>
+      <div className={`${styles.container} ${forceMobile ? 'ursula-mobile-stack' : ''}`}>
         <div className={styles.imageColumn}>
              <img src={image} alt="Client" className={styles.image} />
         
